0470bf361c5319d5a1bfa461289635b8a6aaeb98
PhuongPhan95 <84889231+PhuongPhan95@users.noreply.github.com>
2021-06-04 17:24:34 -0700

    Add files via upload

---
+++ transaction.h
@@ -0,0 +1,20 @@
+
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
+#include <iostream>
+#include <string>
+#include "bintree.h"
+#include "customer.h"
+#include "customerhash.h"
+using namespace std;
+
+class Transaction
+{
+    private:
+    public:
+        Transaction();
+        ~Transaction();
+        virtual void perform(BinTree&, BinTree&, BinTree&, int, string, char, CustomerHash&) { cout << "perform" << endl;}
+};
+
+#endif
