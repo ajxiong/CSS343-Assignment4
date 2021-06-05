30dbf906eac38b944796fda67c0177c5c9df8e3d
PhuongPhan95 <84889231+PhuongPhan95@users.noreply.github.com>
2021-06-04 17:26:10 -0700

    Add files via upload

---
+++ inventory.h
@@ -0,0 +1,18 @@
+#ifndef INVENTORY_H
+#define INVENTORY_H
+#include <iostream>
+#include <string>
+#include "transaction.h"
+#include "customerhash.h"
+using namespace std;
+
+class Inventory : public Transaction
+{
+    private:
+    public:
+        Inventory();
+        ~Inventory();
+        void perform(BinTree&, BinTree&, BinTree&, int, string, char, CustomerHash&);
+};
+
+#endif
