12ac0d67ec123603fd1eb7a518e267f955089931
PhuongPhan95 <84889231+PhuongPhan95@users.noreply.github.com>
2021-06-07 17:12:34 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,11 +7,11 @@ int main() //main driver function
 {
     Business b1;
     
-    BinTree tree1; //for now, just create a tree object here. we will have to
+    BinTree comedyTree, dramaTree, classicTree; //for now, just create a tree object here. we will have to
     //add a tree object data field in our business.h class!
 
     //comment for now
-    //b1.buildMovies(tree1);
+    //b1.buildMovies(comedyTree, dramaTree, classicTree);
     //b1.buildCustomers();
     //b1.processTrans();
     cout << "we are done" << endl;
