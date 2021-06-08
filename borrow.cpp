2c3b20fd31fea1b8a5f71e03785b7de3e09ffbe4
PhuongPhan95 <84889231+PhuongPhan95@users.noreply.github.com>
2021-06-07 21:15:08 -0700

    Update borrow.cpp

--- borrow.cpp
+++ borrow.cpp
@@ -3,7 +3,7 @@
 Borrow::Borrow(){}
 Borrow::~Borrow(){}
 
-void Borrow::display(BinTree&, BinTree&, BinTree&, int, string, char, Hashtable&)
+void Borrow::display(BinTree&, BinTree&, BinTree&, Hashtable&, char, int, string)
 {
 
 }
