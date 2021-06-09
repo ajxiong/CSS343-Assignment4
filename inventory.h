58d9ad70a8077b946fc94fd839135a9cf45bc61f
PhuongPhan95 <84889231+PhuongPhan95@users.noreply.github.com>
2021-06-08 23:38:23 -0700

    Update inventory.h

--- inventory.h
+++ inventory.h
@@ -1,3 +1,19 @@
+// ------------------------------------------------ inventory.h -------------------------------------------------------
+// Programmer Name Course Section Number 
+// Phuong Phan CSS343 section B
+// Creation Date 
+// 6/7/21
+// Date of Last Modification 
+// 6/8/21
+// --------------------------------------------------------------------------------------------------------------------
+// Purpose - a brief statement of the program's function 
+// purpose of inventory.h file is to contain the inventory class, it's methods, encapsulation, and inheritance. 
+// -------------------------------------------------------------------------------------------------------------------- 
+// Notes on specifications, special algorithms, and assumptions. 
+// display is a virtual method that is derived from the Transaction base class. 
+// default constructor and default destructor are included as a pre-cautionary measure. 
+// --------------------------------------------------------------------------------------------------------------------
+
 #ifndef INVENTORY_H
 #define INVENTORY_H
 #include <iostream>
@@ -10,9 +26,9 @@ class Inventory : public Transaction
 {
     private:
     public:
-        Inventory();
-        ~Inventory();
-        void display(BinTree&, BinTree&, BinTree&, Hashtable&, char, int, string);
+        Inventory(); //constructor
+        ~Inventory(); //destructor
+        void display(BinTree&, BinTree&, BinTree&, Hashtable&, char, int, string); //displaying 3 trees
 };
 
 #endif
