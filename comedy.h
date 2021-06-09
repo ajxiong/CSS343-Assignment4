58540d0e88f77822ea1b18bb24d8b67882076f00
PhuongPhan95 <84889231+PhuongPhan95@users.noreply.github.com>
2021-06-08 22:57:47 -0700

    Update comedy.h

--- comedy.h
+++ comedy.h
@@ -1,3 +1,18 @@
+// ------------------------------------------------ comedy.h -------------------------------------------------------
+// Programmer Name Course Section Number 
+// Phuong Phan CSS343 section B
+// Creation Date 
+// 6/7/21
+// Date of Last Modification 
+// 6/8/21
+// --------------------------------------------------------------------------------------------------------------------
+// Purpose - a brief statement of the program's function 
+// purpose of comedy.h file is to contain the Classic class and its methods, encapsulation, and inheritance.  
+// -------------------------------------------------------------------------------------------------------------------- 
+// Notes on specifications, special algorithms, and assumptions. 
+// makeNode is a method that is derived from the Movie base class. 
+// default constructor and default destructor are included as a pre-cautionary measure. 
+// --------------------------------------------------------------------------------------------------------------------
 #ifndef COMEDY_H
 #define COMEDY_H
 #include <iostream>
@@ -9,10 +24,10 @@ using namespace std;
 class Comedy : public Movie
 {
     public:
-        Comedy();
-        ~Comedy();
+        Comedy(); //constructor
+        ~Comedy(); //destructor
 
-        NodeData* makeNode();
+        NodeData* makeNode(); //making a new Node
 
 };
 
