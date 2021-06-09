4304634419f49c5885003879f869833a16e8c534
PhuongPhan95 <84889231+PhuongPhan95@users.noreply.github.com>
2021-06-09 11:52:41 -0700

    Update moviefactory.cpp

--- moviefactory.cpp
+++ moviefactory.cpp
@@ -1,9 +1,41 @@
+// ------------------------------------------------ moviefactory.cpp --------------------------------------
+// Programmer Name Course Section Number 
+// Phuong Phan CSS343 section B
+// Creation Date 
+// 6/1/21
+// Date of Last Modification 
+// 6/9/21
+// --------------------------------------------------------------------------------------------------------------------
+// Purpose - a brief statement of the program's function 
+// purpose of moviefactory.cpp file is to contain the method implementation for the MovieFactory class. 
+// -------------------------------------------------------------------------------------------------------------------- 
+// Notes on specifications, special algorithms, and assumptions. 
+// MovieFactory class is a factory method class that returns a transaction pointer based on the command inputted.
+// if none of the correct commands are inputted, the pointer returns nullptr. 
+// --------------------------------------------------------------------------------------------------------------------
 #include "moviefactory.h"
 
+// ------------------------------------ MovieFactory -------------------------------------------------
+// Pre-conditions: none
+// Post-conditions: none
+// Description: default constructor included as a pre-cautionary measure
+// --------------------------------------------------------------------------------------------------- 
 MovieFactory::MovieFactory() { }
 
+// ------------------------------------ ~MovieFactory ------------------------------------------------
+// Pre-conditions: none
+// Post-conditions: none
+// Description: default constructor included as a pre-cautionary measure
+// --------------------------------------------------------------------------------------------------- 
 MovieFactory::~MovieFactory() { }
 
+// ------------------------------------createComedy---------------------------------------------------
+// Pre-conditions: none
+// Post-conditions: none
+// Description: method returns a Comedy pointer based on the command inputted. since these 
+// classes are derrived from the Transaction class, any Transaction pointer will point to that 
+// specific derrived class object. 
+// --------------------------------------------------------------------------------------------------- 
 Comedy* MovieFactory::createComedy(ifstream& infile) {
         int stock;
         string director, title, year;
@@ -29,6 +61,13 @@ Comedy* MovieFactory::createComedy(ifstream& infile) {
         return comedy;
 }
 
+// ------------------------------------createDrama---------------------------------------------------
+// Pre-conditions: none
+// Post-conditions: none
+// Description: method returns a Drama pointer based on the command inputted. since these 
+// classes are derrived from the Transaction class, any Transaction pointer will point to that 
+// specific derrived class object. 
+// --------------------------------------------------------------------------------------------------- 
 Drama* MovieFactory::createDrama(ifstream& infile) {
         int stock;
         string director, title, year;
@@ -54,6 +93,13 @@ Drama* MovieFactory::createDrama(ifstream& infile) {
         return drama;
 }
 
+// ------------------------------------createClassic--------------------------------------------------
+// Pre-conditions: none
+// Post-conditions: none
+// Description: method returns a Classic pointer based on the command inputted. since these 
+// classes are derrived from the Transaction class, any Transaction pointer will point to that 
+// specific derrived class object. 
+// --------------------------------------------------------------------------------------------------- 
 Classic* MovieFactory::createClassic(ifstream& infile) {
         int stock;
         string director, title, month, year, first, last;
