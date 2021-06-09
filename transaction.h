8ca3cea956d73d17971309449ccd0687277babd1
Nixstream <63818018+ajxiong@users.noreply.github.com>
2021-06-09 09:43:14 -0700

    Update transaction.h

--- transaction.h
+++ transaction.h
@@ -1,3 +1,20 @@
+// ------------------------------------------------ transaction.h -------------------------------------------------------
+// Programmer Name Course Section Number 
+// Alex Xiong CSS343 section B
+// Creation Date 
+// 6/4/21
+// Date of Last Modification 
+// 6/9/21
+// --------------------------------------------------------------------------------------------------------------------
+// Purpose - a brief statement of the program's function 
+// purpose of transaction.h file is to contain the Transaction class, its methods, encapsulation, polymorphism, and 
+// data fields. 
+// -------------------------------------------------------------------------------------------------------------------- 
+// Notes on specifications, special algorithms, and assumptions. 
+// Transaction class is a base class that Borrow, Inventory, History, and Return classes derive from. additionally,
+// since Transaction also includes bintree.h, customer.h, and hashtable.h files, all of its derrived classes also
+// inherit those included class files as well. 
+// --------------------------------------------------------------------------------------------------------------------
 
 #ifndef TRANSACTION_H
 #define TRANSACTION_H
@@ -12,11 +29,29 @@ class Transaction
 {
     private:
     public:
-        Transaction();
-        ~Transaction();
+    
+        // ------------------------------------Transaction---------------------------------------------------
+        // Pre-conditions: none
+        // Post-conditions: none
+        // Description: default constructor included as a pre-cautionary measure
+        // --------------------------------------------------------------------------------------------------- 
+        Transaction(); //end of Transaction
+    
+        // ------------------------------------~Transaction---------------------------------------------------
+        // Pre-conditions: none
+        // Post-conditions: none
+        // Description: default destructor included as a pre-cautionary measure
+        // --------------------------------------------------------------------------------------------------- 
+        ~Transaction(); //end of ~Transaction
+    
+        // ------------------------------------display---------------------------------------------------
+        // Pre-conditions: none
+        // Post-conditions: none
+        // Description: base class pure virtual method. any derrived classes will inherit this method. 
+        // --------------------------------------------------------------------------------------------------- 
         virtual void display(BinTree&, BinTree&, BinTree&, Hashtable&, char, int, string) {
             cout << "Display Transaction" << endl;
-        }
+        } //end of display
 };
 
 #endif
