01703d19f3fb1b15e30d3b6ddc84f82cc79b7e91
PhuongPhan95 <84889231+PhuongPhan95@users.noreply.github.com>
2021-06-08 22:54:08 -0700

    Update classic.h

--- classic.h
+++ classic.h
@@ -1,3 +1,19 @@
+// ------------------------------------------------ classic.h -------------------------------------------------------
+// Programmer Name Course Section Number 
+// Phuong Phan CSS343 section B
+// Creation Date 
+// 6/7/21
+// Date of Last Modification 
+// 6/8/21
+// --------------------------------------------------------------------------------------------------------------------
+// Purpose - a brief statement of the program's function 
+// purpose of classic.h file is to contain the Classic class and its methods, encapsulation, and inheritance.  
+// -------------------------------------------------------------------------------------------------------------------- 
+// Notes on specifications, special algorithms, and assumptions. 
+// makeNode is a method that is derived from the Movie base class. 
+// default constructor and default destructor are included as a pre-cautionary measure. 
+// --------------------------------------------------------------------------------------------------------------------
+
 #ifndef CLASSIC_H
 #define CLASSIC_H
 #include <iostream>
@@ -10,10 +26,12 @@ using namespace std;
 class Classic : public Movie
 {
     public:
-        Classic();
-        ~Classic();
+    
+        Classic(); //constructor
+        
+        ~Classic(); //destructor
 
-        NodeData* makeNode();
+        NodeData* makeNode(); //making a new node
 
 };
 
