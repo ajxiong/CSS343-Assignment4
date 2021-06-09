d1d436e6e85a88d7bc71822f5fbed6bcdfde3861
PhuongPhan95 <84889231+PhuongPhan95@users.noreply.github.com>
2021-06-08 23:29:51 -0700

    Update history.h

--- history.h
+++ history.h
@@ -1,3 +1,20 @@
+
+// ------------------------------------------------ borrow.h -------------------------------------------------------
+// Programmer Name Course Section Number 
+// Phuong Phan CSS343 section B
+// Creation Date 
+// 6/7/21
+// Date of Last Modification 
+// 6/8/21
+// --------------------------------------------------------------------------------------------------------------------
+// Purpose - a brief statement of the program's function 
+// purpose of history.h file is to contain the history class, it's methods, encapsulation, and inheritance. 
+// -------------------------------------------------------------------------------------------------------------------- 
+// Notes on specifications, special algorithms, and assumptions. 
+// display is a virtual method that is derived from the Transaction base class. 
+// default constructor and default destructor are included as a pre-cautionary measure. 
+// --------------------------------------------------------------------------------------------------------------------
+
 #ifndef HISTORY_H
 #define HISTORY_H
 #include "transaction.h"
