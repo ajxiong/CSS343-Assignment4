ee9fab324dbe36df089a4c366fdd667b33ab1c5e
PhuongPhan95 <84889231+PhuongPhan95@users.noreply.github.com>
2021-06-08 22:58:35 -0700

    Update drama.h

--- drama.h
+++ drama.h
@@ -1,3 +1,19 @@
+// ------------------------------------------------ drama.h -------------------------------------------------------
+// Programmer Name Course Section Number 
+// Phuong Phan CSS343 section B
+// Creation Date 
+// 6/7/21
+// Date of Last Modification 
+// 6/8/21
+// --------------------------------------------------------------------------------------------------------------------
+// Purpose - a brief statement of the program's function 
+// purpose of drama.h file is to contain the Drama class and its methods, encapsulation, and inheritance.  
+// -------------------------------------------------------------------------------------------------------------------- 
+// Notes on specifications, special algorithms, and assumptions. 
+// makeNode is a method that is derived from the Movie base class. 
+// default constructor and default destructor are included as a pre-cautionary measure. 
+// --------------------------------------------------------------------------------------------------------------------
+
 #ifndef DRAMA_H
 #define DRAMA_H
 #include <iostream>
@@ -10,9 +26,9 @@ using namespace std;
 class Drama : public Movie
 {
     public:
-        Drama();
-        ~Drama();
-        NodeData* makeNode();
+        Drama(); //constructor
+        ~Drama(); //destructor
+        NodeData* makeNode(); //making a new Node
 };
 
 #endif
