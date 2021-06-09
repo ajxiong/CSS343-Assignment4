79f16767d1207d015e16618fd9bd257fb1face87
Nixstream <63818018+ajxiong@users.noreply.github.com>
2021-06-09 10:01:54 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
+// ------------------------------------------------ main.cpp --------------------------------------------------------
+// Programmer Name Course Section Number 
+// Alex Xiong CSS343 section B
+// Creation Date 
+// 6/7/21
+// Date of Last Modification 
+// 6/9/21
+// --------------------------------------------------------------------------------------------------------------------
+// Purpose - a brief statement of the program's function 
+// purpose of main.cpp is to test the business.cpp class and its functionality. 
+// -------------------------------------------------------------------------------------------------------------------- 
+// Notes on specifications, special algorithms, and assumptions. 
+// main.cpp creates a business object and calls its respective methods. 
+// --------------------------------------------------------------------------------------------------------------------
+
 #include "business.h"
 #include <iostream>
 
 using namespace std;
 
+
+// ------------------------------------main---------------------------------------------------------
+// Pre-conditions: none
+// Post-conditions: none
+// Description: creates a business object and calls its respective methods to thoroughly test
+// its functionality. 
+// --------------------------------------------------------------------------------------------------- 
 int main() //main driver function
 {
     Business b1;
@@ -19,4 +41,4 @@ int main() //main driver function
 
     cout << "We are done" << endl;
     return 0;
-}
+} // end of main
