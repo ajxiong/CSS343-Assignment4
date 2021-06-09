25dd2d66b44ee576705d7f9dbe954532094518d7
PhuongPhan95 <84889231+PhuongPhan95@users.noreply.github.com>
2021-06-08 23:02:47 -0700

    Update history.cpp

--- history.cpp
+++ history.cpp
@@ -1,11 +1,42 @@
+// ------------------------------------------------ history.cpp -------------------------------------------------------
+// Programmer Name Course Section Number 
+// Alex Xiong CSS343 section B
+// Creation Date 
+// 6/7/21
+// Date of Last Modification 
+// 6/8/21
+// --------------------------------------------------------------------------------------------------------------------
+// Purpose - a brief statement of the program's function 
+// purpose of history.cpp file is to contain the implementation of the History class's methods.  
+// -------------------------------------------------------------------------------------------------------------------- 
+// Notes on specifications, special algorithms, and assumptions. 
+// display is a virtual method that is derived from the Transaction base class. 
+// default constructor and default destructor are included as a pre-cautionary measure. 
+// --------------------------------------------------------------------------------------------------------------------
 #include "history.h"
 #include <iostream>
 #include <string>
 
+// ------------------------------------ History --------------------------------------------------------
+// Pre-conditions: none
+// Post-conditions: none
+// Description: default constructor included as a pre-cautionary measure
+// --------------------------------------------------------------------------------------------------- 
 History::History(){}
 
+// ------------------------------------ ~History --------------------------------------------------------
+// Pre-conditions: none
+// Post-conditions: none
+// Description: default destructor included as a pre-cautionary measure
+// ---------------------------------------------------------------------------------------------------
 History::~History(){}
 
+// ------------------------------------display--------------------------------------------------------
+// Pre-conditions: Movie trees and customer hash tables data must be read from .txt files first 
+// Post-conditions: retrieves movie from its respective tree and adds movie and command action to
+// customer history vector, while decreasing that movie's stock.
+// Description: History will be able to print out the transaction history of customers. 
+// --------------------------------------------------------------------------------------------------- 
 void History::display(BinTree& comedyTree, BinTree& dramaTree, BinTree& classicTree, Hashtable& table, char genre, int custID, string name)
 {
     Customer* customer;
